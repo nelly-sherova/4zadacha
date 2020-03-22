684f74afe20274c01d2cce6db98cd82d3617f444
unknown <lvova.nikol@mail.ru>
2020-03-22 14:03:00 +0300

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Введите целое двузначное число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            a = a/10 + a%10*10;
+            Console.WriteLine(a);
+
+        }
+    }
+}
